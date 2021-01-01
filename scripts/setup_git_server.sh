2bb3b5356f7ac3c01dcedb9d98c2c57d2695038f
CoogyEoin <>
2021-01-01 22:01:53 +0000

    Fixing git script

--- scripts/setup_git_server.sh
+++ scripts/setup_git_server.sh
@@ -1,14 +1,15 @@
+
 #!/bin/bash
 
 # This script is used to set up a git server so that you can host your own Repos. To add your public -
 key to the SSH authorised keys use the -pk flag.
 
-TEMP=`getopt -o d: --long domain: -- "$@"`
+TEMP=`getopt -o p:r: --long public_key:repo_name: -- "$@"`
 eval set -- "$TEMP"
 
 while true ; do
     case "$1" in
-        -p|--public-key)
+        -p|--public_key)
             public_key=$2 ; shift 2;;
         -r|--repo_name)
             repo_name=$2 ; shift 2;;
@@ -20,14 +21,14 @@ done
 # Adding user and setting up ssh
 apt install git apache2
 adduser git
-su git
-mkdir ~/.ssh && chmod 700 ~/.ssh
-touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
+
+mkdir /home/git/.ssh && chmod 700 /home/git/.ssh
+touch /home/git/.ssh/authorized_keys && chmod 600 /home/git/.ssh/authorized_keys
 
 # Adding Public Key
 echo "
 ssh-rsa $public_key gsg-keypair
-" > ~/.ssh/authorized_keys
+" > /home/git/.ssh/authorized_keys
 
 # Make Git repos
 mkdir -p /var/www/git/$repo_name.git
@@ -35,3 +36,4 @@ cd /var/www/git/$repo_name.git
 git init --bare
 
 echo "Congrats your git server is good to go. Just push your local repo to git@gitserver:/var/www/git/$repo_name.git"
+
